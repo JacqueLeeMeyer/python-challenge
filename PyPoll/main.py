883bc4ba770d45e17f0e25860be6915394eb2d7b
JacqueLeeMeyer <jacqueline.meyer@llojibwe.net>
2024-01-13 23:19:11 -0600

    Adding PyPoll Files

---
+++ PyPoll/main.py
@@ -0,0 +1,67 @@
+#Import Modules
+import os
+import csv
+import statistics
+
+# Designate path to the PyBank csv file 
+PollDataPath = os.path.join('Module3','Module 3 Challenge','python-challenge','PyPoll','Resources','election_data.csv')
+
+#Read the csv file
+with open(PollDataPath) as Voter_File:
+    VoteReader = csv.reader(Voter_File, delimiter=',')
+    header = next(VoteReader)
+
+#Create empty lists for Votes and Populate it
+    Votes = []
+    for row in VoteReader:
+        Votes.append(row[2])
+
+#Total number of Votes Cast
+    TotalVotes = len(Votes)
+
+#List of Candidates
+    Cand_List = []
+    for name in Votes:
+        if name not in Cand_List:
+                Cand_List.append(name)
+
+#Count the Values for each Candidate
+    Cand_1_Count = Votes.count(Cand_List[0])
+    Cand_2_Count = Votes.count(Cand_List[1])
+    Cand_3_Count = Votes.count(Cand_List[2])
+
+#Percentage of Votes for each Candidate
+    Cand_1_Pct = round((Cand_1_Count/TotalVotes)*100,3)
+    Cand_2_Pct = round((Cand_2_Count/TotalVotes)*100,3)
+    Cand_3_Pct = round((Cand_3_Count/TotalVotes)*100,3)
+
+#Find Winner
+    Winner = statistics.mode(Votes)
+
+#Print Analysis
+    print('Election Results')
+    print('------------------------------')
+    print(f'Total Votes: {TotalVotes}')
+    print('------------------------------')
+    print(f'{Cand_List[0]}: {Cand_1_Pct}% ({Cand_1_Count})')
+    print(f'{Cand_List[1]}: {Cand_2_Pct}% ({Cand_2_Count})')
+    print(f'{Cand_List[2]}: {Cand_3_Pct}% ({Cand_3_Count})')
+    print('------------------------------')
+    print(f'Winner: {Winner}')
+    print('------------------------------')
+
+#Write Output to Text File
+    VoteAnalysisPath = os.path.join('Module3','Module 3 Challenge','python-challenge','PyPoll','Analysis','PyPoll_Analysis_JMeyer.txt')
+
+    with open(VoteAnalysisPath, 'w') as Text_File:
+        print('Election Results', file=Text_File)
+        print('------------------------------', file=Text_File)
+        print(f'Total Votes: {TotalVotes}', file=Text_File)
+        print('------------------------------', file=Text_File)
+        print(f'{Cand_List[0]}: {Cand_1_Pct}% ({Cand_1_Count})', file=Text_File)
+        print(f'{Cand_List[1]}: {Cand_2_Pct}% ({Cand_2_Count})', file=Text_File)
+        print(f'{Cand_List[2]}: {Cand_3_Pct}% ({Cand_3_Count})', file=Text_File)
+        print('------------------------------', file=Text_File)
+        print(f'Winner: {Winner}', file=Text_File)
+        print('------------------------------', file=Text_File)
+        
--- README.md
+++ README.md
@@ -1,11 +1,19 @@
 # python-challenge
 # Module 3 Challenge
-# Part 1: PyBank
-# Sources I used to help write me code:
-#   https://www.geeksforgeeks.org/precision-handling-python/
-#	    To find rounding float (Line 52 in PyBank/main.py)
+    # Part 1: PyBank
+        # Sources I used to help write me code:
+        #   https://www.geeksforgeeks.org/precision-handling-python/
+        #	    To find rounding float 
+        #           (Line 52 in PyBank/main.py)
 
-#   https://stackoverflow.com/questions/36571560/directing-print-output-to-a-txt-file
-#	    To Print Python results to a Text File (Lines 77-84 in PyBank/main.py)
+        #   https://stackoverflow.com/questions/36571560/directing-print-output-to-a-txt-file
+        #	    To Print Python results to a Text File 
+        #           (Lines 77-84 in PyBank/main.py)
 
-
+    #Part 2: PyPoll
+        #Source I used to help write me code:
+        #   https://www.geeksforgeeks.org/python-statistics-mode-function/
+        #        To understand how to use mode function in Python
+        #           (Line 39 in PyPoll/main.py)
+    
+    
